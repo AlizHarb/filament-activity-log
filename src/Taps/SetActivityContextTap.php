0b8426d15305a1757703e5d1c8bac24c21f1d960
Ali Harb <harbzali@gmail.com>
2025-12-08 13:15:31 -0500

    Fix: Resolve PHPStan undefined property error in SetActivityContextTap

--- src/Taps/SetActivityContextTap.php
+++ src/Taps/SetActivityContextTap.php
@@ -9,6 +9,7 @@ class SetActivityContextTap
 {
     public function __invoke(Activity $activity, string $eventName, ?Model $subject = null, ?Model $causer = null, ?array $properties = null): void
     {
+        /** @var \Spatie\Activitylog\Models\Activity $activity */
         $activity->properties = $activity->properties->merge([
             'ip_address' => request()->ip(),
             'user_agent' => request()->userAgent(),
